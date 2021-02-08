e85575f00dc38256109213a1b6a1e775673f5cc2
Adeynack <Adeynack@users.noreply.github.com>
2021-02-08 17:14:10 +0100

    Do not create event ID on creation (#2)
    * initial commit for do not use UUID for Event ID
    
    * do not generate UUID IDs at creation

--- lib/yael/dispatch_job.rb
+++ lib/yael/dispatch_job.rb
@@ -7,7 +7,7 @@ module Yael
     queue_as :dispatch
 
     def perform(name:, stream:, payload:, created_at:, persist: true)
-      event = Event.new id: SecureRandom.uuid, name: name, stream: stream, payload: payload, created_at: created_at
+      event = Event.new name: name, stream: stream, payload: payload, created_at: created_at
       event.save! if persist
       Yael::Bus.shared.process(event)
     end
